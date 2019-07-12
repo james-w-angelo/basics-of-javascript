16e962c41e290649565f196a311eb7508a80bbed
james-w-angelo <52141617+james-w-angelo@users.noreply.github.com>
2019-07-12 10:40:33 -0400

    Create exercise-nine.js

---
+++ exercise-nine.js
@@ -0,0 +1,9 @@
+var firstExpression = 100 + 4 * 11;
+
+var secondExpression = (100 + 4) * 11;
+
+var thirdExpression = (312 + 13) % 10 * 5;
+
+var fourthExpression = 312 + 13 % 10 * 5;
+
+var fourthExpression = 312 + (13 % 10 * 5);
